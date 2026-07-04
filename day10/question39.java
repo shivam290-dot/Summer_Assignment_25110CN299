2d2ae5fca93955d578f554ee8ec5e8c42570aca7
Shivam Singh <ishivamsinghtp@gmail.com>
2026-07-04 13:16:15 +0530

    day10

--- day10/question39.java
+++ day10/question39.java
@@ -1,5 +1,26 @@
 package day10;
+import java.util.Scanner;
+public class question39{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
+        int n = sc.nextInt();
 
-public class question39 {
-    
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <=n - i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+
+            for(int j = i - 1; j >= 1; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
